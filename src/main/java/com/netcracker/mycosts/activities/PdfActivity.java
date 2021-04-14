acfbb583baf33133a07c8034b1034986b4c35a5b
neo_nestea <yakova.nastya@yandex.ru>
2021-04-14 22:27:21 +0300

    pdf

---
+++ src/main/java/com/netcracker/mycosts/activities/PdfActivity.java
@@ -0,0 +1,70 @@
+package com.netcracker.mycosts.activities;
+
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+import lombok.SneakyThrows;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+@Component
+
+public class PdfActivity {
+    @SneakyThrows
+    @Scheduled(fixedRate = 60000)
+    public void createDocument() {
+        Document document = new Document(PageSize.A4, 20, 20, 20, 20);
+        PdfWriter.getInstance(document, new FileOutputStream("file.pdf"));
+
+        document.open();
+        document.add( new Paragraph( "Costs by MONTH" ) );
+        //document.add( Chunk.NEWLINE );
+        document.add( Chunk.NEWLINE );
+        PdfPTable table = new PdfPTable(3);
+        addTableHeader(table);
+        addRows(table);
+        addCustomRows(table);
+
+        document.add(table);
+        document.close();
+    }
+
+
+    private void addTableHeader(PdfPTable table) {
+        Stream.of("Account", "Category", "Total amount")
+                .forEach(columnTitle -> {
+                    PdfPCell header = new PdfPCell();
+                    header.setBackgroundColor(BaseColor.LIGHT_GRAY);
+                    header.setBorderWidth(2);
+                    header.setPhrase(new Phrase(columnTitle));
+                    table.addCell(header);
+                });
+    }
+
+    private void addRows(PdfPTable table) {
+        table.addCell("row 1, col 1");
+        table.addCell("row 1, col 2");
+        table.addCell("row 1, col 3");
+    }
+
+    private void addCustomRows(PdfPTable table)
+            throws URISyntaxException, BadElementException, IOException {
+
+        PdfPCell horizontalAlignCell = new PdfPCell(new Phrase("row 2, col 2"));
+        horizontalAlignCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        table.addCell(horizontalAlignCell);
+
+        PdfPCell verticalAlignCell = new PdfPCell(new Phrase("row 2, col 3"));
+        verticalAlignCell.setVerticalAlignment(Element.ALIGN_BOTTOM);
+        table.addCell(verticalAlignCell);
+    }
+}
