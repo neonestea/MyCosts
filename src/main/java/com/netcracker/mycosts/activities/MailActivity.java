acf71395e6fc39bd52778421cbba2dfb3c5e7fcf
neo_nestea <yakova.nastya@yandex.ru>
2021-04-15 20:32:14 +0300

    added email

---
+++ src/main/java/com/netcracker/mycosts/activities/MailActivity.java
@@ -0,0 +1,18 @@
+package com.netcracker.mycosts.activities;
+
+import com.netcracker.mycosts.services.MailSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MailActivity {
+    @Autowired
+    private MailSender mailSender;
+
+    @Scheduled(fixedRate = 60000)
+    public void sendEmail(){
+        String message = String.format("Hello");
+        mailSender.send("yankova.nastya@yandex.ru", "Costs by april", message);
+    }
+}
